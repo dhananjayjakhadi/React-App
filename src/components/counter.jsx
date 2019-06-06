7e5f30207d15fec78ad9dcb3377ac5b20178014f
dhananjayjakhadi <31828887+dhananjayjakhadi@users.noreply.github.com>
2019-06-05 21:47:19 -0400

    Specifying children in component. We can either use React.Fragment or div

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Counter extends Component {
-    render() { 
-        return <h1>Welcom to React Counter App</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import Counter from './components/counter';
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
